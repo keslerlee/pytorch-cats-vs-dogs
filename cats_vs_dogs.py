4d89ad85b45d3853bce4305631aa228e45c9ab4f
keslerlee <tokeslerlee@gmail.com>
2025-11-23 21:49:52 -0700

    Update cats_vs_dogs.py

--- cats_vs_dogs.py
+++ cats_vs_dogs.py
@@ -1,4 +1,16 @@
 import torch
+import torchvision
+import torchvision.transforms as transforms
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
 print(f"Using device: {device}")
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+train_dataset_full = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+test_dataset_full = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
